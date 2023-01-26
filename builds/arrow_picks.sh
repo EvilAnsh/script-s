e02dd8ada8d18d1e8d6d4dbe793a5d9e2eeea06c
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2023-01-26 13:15:45 +0800

    arrow_picks: Prebuilt kernel patch
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- builds/arrow_picks.sh
+++ builds/arrow_picks.sh
@@ -7,6 +7,11 @@ cd frameworks/av
 git fetch https://review.arrowos.net/ArrowOS/android_frameworks_av refs/changes/57/18657/6 && git cherry-pick FETCH_HEAD
 cd ../..
 
+# vendor/arrow: Do not build kernel from source if prebuilt is defined
+cd vendor/arrow
+git am <(https://pastebin.com/raw/GnqV3Knb)
+cd ../..
+
 # LineageOS Aperture
 rm -rfv packages/apps/Camera2
 test -d packages/apps/Aperture || git clone https://github.com/LineageOS/android_packages_apps_Aperture packages/apps/Aperture
