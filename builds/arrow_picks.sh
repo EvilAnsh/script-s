f9a748406af82a6da5fa26d306fb787612401455
SamarV-121 <samarvispute121@pm.me>
2023-01-26 14:54:05 +0800

    arrow_picks: Check if patch is already applied
    * and use git am for frameworks/av
    
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- builds/arrow_picks.sh
+++ builds/arrow_picks.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 readonly ARROW_ROOT=$HOME/arrow
+readonly SCRIPT_NAME=$0
 cd "$ARROW_ROOT" || exit 1
 
+function print {
+    echo "$SCRIPT_NAME: $*"
+}
 # frameworks/av: alps codecs stuff
 cd frameworks/av
-git fetch https://review.arrowos.net/ArrowOS/android_frameworks_av refs/changes/57/18657/6 && git cherry-pick FETCH_HEAD
+if patch -p1 --dry-run -R <(curl -sL https://github.com/ArrowOS/android_frameworks_av/commit/1fb1c48309cf01deb9e3f8253cb7fa5961c25595.patch) >/dev/null; then
+    git am <(curl -sL https://github.com/ArrowOS/android_frameworks_av/commit/1fb1c48309cf01deb9e3f8253cb7fa5961c25595.patch)
+else
+    print "frameworks/av: alps commit already applied, skipping"
+fi
 cd ../..
 
 # vendor/arrow: Do not build kernel from source if prebuilt is defined
 cd vendor/arrow
-git am <(https://pastebin.com/raw/GnqV3Knb)
+if patch -p1 --dry-run -R <(curl -sL https://pastebin.com/raw/GnqV3Knb); then
+    git am <(curl -sL https://pastebin.com/raw/GnqV3Knb)
+else
+    print "vendor/arrow: kernel patch already applied, skipping"
+fi
 cd ../..
 
 # LineageOS Aperture
