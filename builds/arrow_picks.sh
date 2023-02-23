5a8cf03b32e31da24121cbc5fdd53f0e40f89c34
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2023-02-23 17:21:00 +0800

    Remove unneeded patch

--- builds/arrow_picks.sh
+++ builds/arrow_picks.sh
@@ -27,13 +27,13 @@ apply_patch vendor/arrow https://pastebin.com/raw/GnqV3Knb
 #apply_patch frameworks/base https://github.com/realme-mt6785-devs/android_frameworks_base/commit/7d626a51c37bf40dcceeae0c52afc4b5fbf5203a.patch
 
 # Add bluetooth and sdk_sandbox to default key map
-apply_patch build https://github.com/LineageOS/android_build/commit/483f3cf277485c9eaeaf5e025836ea0271574a63.patch use_patch
+#apply_patch build https://github.com/LineageOS/android_build/commit/483f3cf277485c9eaeaf5e025836ea0271574a63.patch use_patch
 
 # fixup! bluetooth: abandon app local certificate
-apply_patch packages/modules/Bluetooth https://github.com/LineageOS/android_packages_modules_Bluetooth/commit/5d2632ad83e4566064f3d3fbd6af5b60fa00b71c.patch
+#apply_patch packages/modules/Bluetooth https://github.com/LineageOS/android_packages_modules_Bluetooth/commit/5d2632ad83e4566064f3d3fbd6af5b60fa00b71c.patch
 
 # bluetooth: abandon app local certificate
-apply_patch packages/modules/Bluetooth https://github.com/LineageOS/android_packages_modules_Bluetooth/commit/00cecba4b5d276c555cd26dfc5394a9fbfd33b6e.patch
+#apply_patch packages/modules/Bluetooth https://github.com/LineageOS/android_packages_modules_Bluetooth/commit/00cecba4b5d276c555cd26dfc5394a9fbfd33b6e.patch
 
 
 # LineageOS Aperture (added to local manifest)
